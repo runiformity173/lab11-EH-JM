bf7d9e3d9d2b183c340a90e29bdfbe4efaa6c1ed
runiformity173 <eli.z.harrison173@gmail.com>
2025-11-15 13:53:31 -0500

    finished unit tests

--- test_calculator.py
+++ test_calculator.py
@@ -11,18 +11,22 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(calculator.add(0, 0), 0)
 
     def test_subtract(self): # 3 assertions
-        self.assertEqual(calculator.sub(10, 4), 6)
-        self.assertEqual(calculator.sub(0, 5), -5)
-        self.assertEqual(calculator.sub(-3, -2), -1)
+        self.assertEqual(calculator.subtract(10, 4), 6)
+        self.assertEqual(calculator.subtract(0, 5), -5)
+        self.assertEqual(calculator.subtract(-3, -2), -1)
 
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(calculator.mul(5,4),20)
+        self.assertEqual(calculator.mul(-8,2),-16)
+        self.assertAlmostEqual(calculator.mul(5.4,-5),-27.0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
+    def test_divide(self): # 3 assertions
+        self.assertAlmostEqual(calculator.div(5.0,-5),-1.0)
+        self.assertAlmostEqual(calculator.div(5.4,2),2.7)
+        self.assertAlmostEqual(calculator.div(-100.0,-5),20.0)
     # ##########################
 
     ######## Partner 2
@@ -31,33 +35,33 @@ class TestCalculator(unittest.TestCase):
             calculator.div(5, 0)
 
     def test_logarithm(self): # 3 assertions
-        self.assertAlmostEqual(calculator.log(2, 8), 3.0)
-        self.assertAlmostEqual(calculator.log(10, 100), 2.0)
-        self.assertAlmostEqual(calculator.log(4, 64), 3.0)
+        self.assertAlmostEqual(calculator.logarithm(2, 8), 3.0)
+        self.assertAlmostEqual(calculator.logarithm(10, 100), 2.0)
+        self.assertAlmostEqual(calculator.logarithm(4, 64), 3.0)
 
     def test_log_invalid_base(self): # 1 assertion
         with self.assertRaises(ValueError):
-            calculator.log(1, 5)
+            calculator.logarithm(1, 5)
 
     # ##########################
     
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    ####### Partner 1
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            calculator.logarithm(0, 5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        self.assertAlmostEqual(calculator.hypotenuse(3,4),5.0)
+        self.assertAlmostEqual(calculator.hypotenuse(-6,-8),10.0)
+        self.assertAlmostEqual(calculator.hypotenuse(-5,12),13.0)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ValueError):
+           calculator.square_root(-5)
+        # Test basic function
+        self.assertAlmostEqual(calculator.square_root(4),2.0)
+        self.assertAlmostEqual(calculator.square_root(6.25),2.5)
+    #########################
 
 # Do not touch this
 if __name__ == "__main__":
