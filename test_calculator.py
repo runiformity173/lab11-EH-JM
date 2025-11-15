6cca0cb4934cd457c8337c54eabf9d3740de29c9
Marcos <4804190562@ocps.k12.fl.us>
2025-11-15 04:13:16 -0500

    test cases

--- test_calculator.py
+++ test_calculator.py
@@ -27,8 +27,8 @@ class TestCalculator(unittest.TestCase):
 
     ######## Partner 2
     def test_divide_by_zero(self): # 1 assertion
-    with self.assertRaises(ZeroDivisionError):
-        calculator.div(5, 0)
+        with self.assertRaises(ZeroDivisionError):
+            calculator.div(5, 0)
 
     def test_logarithm(self): # 3 assertions
         self.assertAlmostEqual(calculator.log(2, 8), 3.0)
